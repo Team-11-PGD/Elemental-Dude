b45de1339c5fc22cd5f21fe91f1bbaa00d3012ed
lukem2320 <lukemarcos23@gmail.com>
2021-10-04 15:08:10 +0200

    main menu en ui manager

---
+++ Assets/UIManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class UIManager : MonoBehaviour
+{
+ public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void QuitGame()
+    {
+       
+        Application.Quit();
+        UnityEditor.EditorApplication.isPlaying = false;
+
+    }
+    public void GoToMainMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+}
