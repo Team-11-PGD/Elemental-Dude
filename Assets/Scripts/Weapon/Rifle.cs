2c8645219419bff168167d4989a259afbc039771
MeesMD <meesmilan@hotmail.com>
2021-10-07 15:59:16 +0200

    Reload times and rifle weapon
    Made the reload time but is not finished yet, and added the rifle behaviour so you can use it in the game

--- Assets/Scripts/Weapon/Rifle.cs
+++ Assets/Scripts/Weapon/Rifle.cs
@@ -6,12 +6,22 @@ public class Rifle : Weapon
 {
     void Start()
     {
-        bulletAmount = 30;
+        bulletAmount = 5;
+        maxBullets = 30;
         reloadTime = 3;
+        timeToFire = 0;
+        fireInterval = 0.4f;
     }
 
     void Update()
     {
-        
+        //Debug.Log(bulletAmount);
+        if (Input.GetButton("Fire1") && Time.time >= timeToFire && canFire)
+        {
+            timeToFire = Time.time + fireInterval;
+            Shoot();
+        }
+
+
     }
 }
--- Assets/Scripts/Weapon/Weapon.cs
+++ Assets/Scripts/Weapon/Weapon.cs
@@ -4,48 +4,56 @@ using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
-    public int bulletAmount;
-    public int reloadTime;
     public Transform spawnBulletPos;
     public Transform bulletPrefab;
     public Camera playerCam;
 
-    private float timeToFire = 0;
-    private float fireInterval = 0.4f;
-    private Vector3 mouseWorldPos;
+    public int bulletAmount;
+    public int maxBullets;
+    public int reloadTime;
+    public float timeToFire;
+    public float fireInterval;
+    public bool canFire = true;
 
-    public enum WeaponType
-    {
-        Rifle,
-        Shotgun,
-        RPG
-    }
+    private Vector3 aimPoint;
 
     void Start()
     {
-
     }
 
     void Update()
     {
-        if (Input.GetButton("Fire1") && Time.time >= timeToFire)
-        {
-            timeToFire = Time.time + fireInterval;
-            Shoot();
-        }
-    }
-
-    private void Shoot()
-    {
-        RaycastHit hit;
-        if (Physics.Raycast(playerCam.transform.position, playerCam.transform.forward, out hit, 999f))
-        {
-            Debug.Log(hit.transform.name);
-            mouseWorldPos = hit.point;
-        }
         Debug.DrawRay(playerCam.transform.position, playerCam.transform.forward * 50, Color.green);
 
-        Vector3 aimDir = (mouseWorldPos - spawnBulletPos.position).normalized;
+        if (Input.GetKeyDown(KeyCode.R) && !canFire)
+        {
+            Debug.Log("bigballs");
+        }
+    }
+
+    public virtual void Shoot()
+    {
+        if (bulletAmount <= 0)
+        {
+            canFire = false;
+        }
+        else
+        {
+            bulletAmount -= 1;
+        }
+
+        RaycastHit hit;
+        if (Physics.Raycast(playerCam.transform.position, playerCam.transform.forward, out hit, 50f))
+        {
+            //Debug.Log(hit.transform.name);
+            aimPoint = hit.point;
+        }
+        Vector3 aimDir = (aimPoint - spawnBulletPos.position).normalized;
         Instantiate(bulletPrefab, spawnBulletPos.position, Quaternion.LookRotation(aimDir, Vector3.up));
     }
+
+    public virtual void Reload()
+	{
+        Debug.Log("PRESS R TO RELOAD");
+	}
 }
