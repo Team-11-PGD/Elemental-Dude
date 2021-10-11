64c2a2f7831596b1ceb081e1292b70f11dedf974
Joshua Knaven <joshua.knaven@gmail.com>
2021-10-11 17:09:57 +0200

    Start of BossAI part 2

---
+++ Assets/Scripts/BossAI.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossAI : StateMachine
+{
+    [SerializeField]
+    StateOptions startState = StateOptions.Attacking;
+
+    public enum StateOptions
+    {
+        MoveToPlayer,
+        Attacking,
+        Defending
+    }
+
+    protected void Start()
+    {
+        states.Add((int)StateOptions.MoveToPlayer, gameObject.AddComponent<BossMoveToPlayerState>());
+        states.Add((int)StateOptions.Attacking, gameObject.AddComponent<ExampleState2>());
+        states.Add((int)StateOptions.Defending, gameObject.AddComponent<ExampleState1>());
+
+        // You also need to call the StateMachineSetup method so the statemachine works.
+        // Do this after you've added all the states!
+        StateMachineSetup((int)startState);
+    }
+}
