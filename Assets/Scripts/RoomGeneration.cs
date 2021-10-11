15adb760ef3d35980a46a4ca96c6d2e306eb167d
Joshua Knaven <joshua.knaven@gmail.com>
2021-10-11 13:58:56 +0200

    RoomGeneration updated to fit discussed design
    - RoomGeneration will now allow for duplicate elements in a run but prevent them from showing right after one another
    - RoomGeneration will also generate options for paths to the next level

--- Assets/Scripts/RoomGeneration.cs
+++ Assets/Scripts/RoomGeneration.cs
@@ -1,7 +1,9 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Linq;
 
 static public class RoomGeneration
 {
@@ -10,55 +12,52 @@ static public class RoomGeneration
     /// </summary>
     public static List<ElementMain.ElementType> CurrentElements { get; private set; } = new List<ElementMain.ElementType>();
 
-    static List<ElementMain.ElementType> availableElements = new List<ElementMain.ElementType>();
+    /// <summary>
+    /// A array that contains all options for the next level
+    /// </summary>
+    public static List<ElementMain.ElementType>[] NextElements { get; private set; } = new List<ElementMain.ElementType>[PATH_OPTIONS];
+
+    static List<ElementMain.ElementType> previousElements = new List<ElementMain.ElementType>();
+    const int PATH_OPTIONS = 2;
 
     /// <summary>
-    /// Reset element list
+    /// Load next level element data and scene
     /// </summary>
-    static void RefillAvailableElements()
+    /// <param name="elementTypes"> Types of elements to load the next level with </param>
+    /// <param name="amountOfElementsNextRound"> Amount of elements in combi for next level </param>
+    public static void LoadNextLevel(List<ElementMain.ElementType> elementTypes, int amountOfElementsNextRound)
     {
-        availableElements = new List<ElementMain.ElementType>
-        {
-             ElementMain.ElementType.Water,
-             ElementMain.ElementType.Fire,
-             ElementMain.ElementType.Air,
-             ElementMain.ElementType.Earth
-        };
-    }
+        previousElements = CurrentElements;
+        CurrentElements = elementTypes;
 
-    /// <summary>
-    /// Load next room with new random elements
-    /// </summary>
-    /// <param name="amountOfElements"> Amount of elements to load next room with </param>
-    public static void LoadNextRoom(int amountOfElements)
-    {
-        // Refill element list for next rounds.
-        if (availableElements.Count <= 0)
+        // Get random elements for each path
+        for (int i = 0; i < PATH_OPTIONS; i++)
         {
-            RefillAvailableElements();
+            List<ElementMain.ElementType> randomElements;
+
+            do
+            {
+                // Create a list with all possible elements
+                List<ElementMain.ElementType> availableElements = Enum.GetValues(typeof(ElementMain.ElementType)).OfType<ElementMain.ElementType>().ToList();
+                availableElements.Remove(ElementMain.ElementType.None); 
+
+                randomElements = new List<ElementMain.ElementType>();
+
+                // Find random elements
+                for (int j = 0; j < amountOfElementsNextRound; j++)
+                {
+                    int rand = UnityEngine.Random.Range(0, availableElements.Count);
+                    randomElements.Add(availableElements[rand]);
+                    availableElements.RemoveAt(rand);
+                }
+
+                // Sort the list so that different order doesn't matter when checking repeating levels
+                randomElements.Sort();
+            } while (randomElements == previousElements); // Make sure the new elements dont repeat the previous level
+
+            NextElements[PATH_OPTIONS] = randomElements;
         }
 
-        string message = "Load biome with";
-
-        CurrentElements.Clear();
-
-        // Get random elements
-        for (int i = 0; i < amountOfElements; i++)
-        {
-            // If to many elements were asked for break loop
-            if (availableElements.Count <= 0) break;
-
-            int rand = Random.Range(0, availableElements.Count);
-            ElementMain.ElementType element = availableElements[rand];
-
-            CurrentElements.Add(element);
-            message += " " + element;
-
-            // Remove element from options
-            availableElements.RemoveAt(rand);
-        }
-
-        Debug.Log(message + " element");
-
+        Debug.Log(NextElements);
     }
 }
