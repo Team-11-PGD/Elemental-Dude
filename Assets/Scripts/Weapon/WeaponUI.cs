6e691f0553bc67e0cae9ef7cfaff96772ee57fc1
RTosk <71409186+RTosk@users.noreply.github.com>
2021-12-01 14:04:38 +0100

    improved weapon Ui
    added a new ui for guns

--- Assets/Scripts/Weapon/WeaponUI.cs
+++ Assets/Scripts/Weapon/WeaponUI.cs
@@ -7,34 +7,64 @@ public class WeaponUI : MonoBehaviour
 {
     // Start is called before the first frame update
     [SerializeField]
-    private Image Rifle;
+    private Image SelectorRifle;
     [SerializeField]
-    private Image RPG;
+    private Image SelectorShotgun;
     [SerializeField]
-    private Image Shotgun;
+    private Image SelectorRPG;
+
     [SerializeField]
     private WeaponSwitcher weaponSwitcher;
 
-        // Update is called once per frame
-        void Update()
-        {
+    [SerializeField]
+    private ElementMain elementRifle;
+    [SerializeField]
+    private ElementMain elementShotgun;
+    [SerializeField]
+    private ElementMain elementRPG;
+
+    // Update is called once per frame
+    void Update()
+    {
         if (weaponSwitcher.curWeapon == Weapon.WeaponTypes.Rifle)
         {
-            Rifle.enabled = true;
+            SelectorRifle.enabled = true;
         }
-        else Rifle.enabled = false;
+        else SelectorRifle.enabled = false;
 
-            if (weaponSwitcher.curWeapon == Weapon.WeaponTypes.RPG)
-            {
-                RPG.enabled = true;
-            }
-        else RPG.enabled = false;
+        if (weaponSwitcher.curWeapon == Weapon.WeaponTypes.RPG)
+        {
+            SelectorRPG.enabled = true;
+        }
+        else SelectorRPG.enabled = false;
 
         if (weaponSwitcher.curWeapon == Weapon.WeaponTypes.Shotgun)
-            {
-                Shotgun.enabled = true;
-            }
-        else Shotgun.enabled = false;
+        {
+            SelectorShotgun.enabled = true;
+        }
+        else SelectorShotgun.enabled = false;
+
+        //color changer for the Ui.
+        if (weaponSwitcher.curWeapon == Weapon.WeaponTypes.Rifle)
+        {
+            if (elementRifle.currentType == ElementMain.ElementType.Water) SelectorRifle.color = Color.blue;
+            if (elementRifle.currentType == ElementMain.ElementType.Air) SelectorRifle.color = Color.gray;
+            if (elementRifle.currentType == ElementMain.ElementType.Fire) SelectorRifle.color = Color.red;
+            if (elementRifle.currentType == ElementMain.ElementType.Earth) SelectorRifle.color = new Color(0.59f, 0.38f, 0.21f);
+        }
+        if (weaponSwitcher.curWeapon == Weapon.WeaponTypes.Shotgun)
+        {
+            if (elementShotgun.currentType == ElementMain.ElementType.Water) SelectorShotgun.color = Color.blue;
+            if (elementShotgun.currentType == ElementMain.ElementType.Air) SelectorShotgun.color = Color.gray;
+            if (elementShotgun.currentType == ElementMain.ElementType.Fire) SelectorShotgun.color = Color.red;
+            if (elementShotgun.currentType == ElementMain.ElementType.Earth) SelectorShotgun.color = new Color(0.59f, 0.38f, 0.21f);
+        }
+        if (weaponSwitcher.curWeapon == Weapon.WeaponTypes.RPG)
+        {
+            if (elementRPG.currentType == ElementMain.ElementType.Water) SelectorRPG.color = Color.blue;
+            if (elementRPG.currentType == ElementMain.ElementType.Air) SelectorRPG.color = Color.gray;
+            if (elementRPG.currentType == ElementMain.ElementType.Fire) SelectorRPG.color = Color.red;
+            if (elementRPG.currentType == ElementMain.ElementType.Earth) SelectorRPG.color = new Color(0.59f, 0.38f, 0.21f);
+        }
     }
-    
 }
