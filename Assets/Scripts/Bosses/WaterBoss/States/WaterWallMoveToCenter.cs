6c9f57f642cc10247d129abae524be753b5eda4d
Chris Desktop <chrishuider@gmail.com>
2021-12-18 16:04:38 +0100

    Water boss arena flooding working

--- Assets/Scripts/Bosses/WaterBoss/States/WaterWallMoveToCenter.cs
+++ Assets/Scripts/Bosses/WaterBoss/States/WaterWallMoveToCenter.cs
@@ -4,20 +4,15 @@ using UnityEngine;
 
 public class WaterWallMoveToCenter : WaterWaveScript
 {
-    [SerializeField]
-    private Transform roomCenter;
 
-    private Vector3 moveToCenter;
+    public Transform roomCenter;
 
-    #region Dev-tool
-    [SerializeField]
-    public bool startAppear, start1, start2;
-    #endregion
+    protected Vector3 moveToCenter;
 
-    private bool stage1Moving, stage2Moving;
+    protected bool stage1Moving, stage2Moving;
 
     [SerializeField]
-    private float stage1Distance = 30, stage2Distance = 20;
+    private float stage1Distance, stage2Distance;
 
     protected override void Start()
     {
@@ -25,33 +20,15 @@ public class WaterWallMoveToCenter : WaterWaveScript
         canDamage = true;
     }
 
-    void Update()
+    protected virtual void Update()
     {
-        #region Dev-tool
-        if (startAppear)
-        {
-            StartCoroutine(Appear());
-            startAppear = false;
-        }
-        if (start1)
-        {
-            StartStage(1);
-            start1 = false;
-        }
-        if (start2)
-        {
-            StartStage(2);
-            start2 = false;
-        }
-        #endregion
-
         float centerAndWallDistance = (roomCenter.position - transform.position).magnitude;
 
         if (stage1Moving && centerAndWallDistance <= stage1Distance) StopClosingIn(1);
         if (stage2Moving && centerAndWallDistance <= stage2Distance) StopClosingIn(2);
     }
 
-    public IEnumerator Appear()
+    protected virtual IEnumerator Appear()
     {
         rigidBody.AddForce(Vector3.up * waveSpeed);
         yield return new WaitUntil(() => transform.position.y -6 >= roomCenter.position.y); //The -6 is so it lines up with the bottom and not the middle
@@ -62,8 +39,9 @@ public class WaterWallMoveToCenter : WaterWaveScript
 
     public void StartStage(int stage)
     {
-        if(stage == 1) stage1Moving = true;
-        if(stage == 2) stage2Moving = true;
+        if (stage == 0) StartCoroutine(Appear());
+        if (stage == 1) stage1Moving = true;
+        if (stage == 2) stage2Moving = true;
         rigidBody.AddForce(moveToCenter);
     }
 
