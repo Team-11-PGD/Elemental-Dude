5a25a12ff24abd97713870075b9df55ae30323e9
IngoBlok <78639304+IngoBlok@users.noreply.github.com>
2021-12-21 13:28:32 +0100

    FIxed lavastream shooting random direction

--- Assets/Scripts/Bosses/FireBoss/States/BossDefendingLavaStreamState.cs
+++ Assets/Scripts/Bosses/FireBoss/States/BossDefendingLavaStreamState.cs
@@ -13,6 +13,8 @@ public class BossDefendingLavaStreamState : FireBossState
     [SerializeField]
     float groundbreakTime = 3f;
     [SerializeField]
+    float lookAtTime = 1f;
+    [SerializeField]
     float groundbreakDamage = 0.01f;
 
     public override void Enter(int previousStateId)
@@ -30,6 +32,7 @@ public class BossDefendingLavaStreamState : FireBossState
 
     IEnumerator GroundbreakTimer()
     {
+        yield return new WaitForSecondsRealtime(lookAtTime);
         for (int i = 0; i < instantiateAmount; i++)
         {
             GameObject groundbreakInstance = Instantiate(groundbreakPrefab, groundbreakStartPosition.position, context.transform.rotation, null);
