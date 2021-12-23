5af888904e9aefe2e7665d00e73559f9e51b8631
Chris Desktop <chrishuider@gmail.com>
2021-12-23 16:07:37 +0100

    Working on doors

---
+++ Assets/Scripts/Levels/Door.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject leftDoor, rightDoor;
+
+    bool test;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /*public IEnumerator openDoor()
+    {
+        yield return new WaitUntil(test) ;
+    }*/
+}
