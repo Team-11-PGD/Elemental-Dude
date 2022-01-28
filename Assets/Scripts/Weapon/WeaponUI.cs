af31edff897535a89ef3678fc3a79f6655adb21a
RTosk <71409186+RTosk@users.noreply.github.com>
2022-01-28 17:14:19 +0100

    yes

--- Assets/Scripts/Weapon/WeaponUI.cs
+++ Assets/Scripts/Weapon/WeaponUI.cs
@@ -6,33 +6,15 @@ using UnityEngine.UI;
 
 public class WeaponUI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    [SerializeField]
-    private Image SelectorRifle;
-    [SerializeField]
-    private Image SelectorShotgun;
-    [SerializeField]
-    private Image SelectorRPG;
 
     [SerializeField]
-    private Image rifleEffective;
-    [SerializeField]
-    private Image shotgunEffective;
-    [SerializeField]
-    private Image rgpEffective;
-    [SerializeField]
-    private Image rifleCurrent;
-    [SerializeField]
-    private Image shotgunCurrent;
-    [SerializeField]
-    private Image rpgCurrent;
+    private Image Selector;
 
     [SerializeField]
-    private Text RifleMod;
+    private Image rifleImage, shotgunImage, rpgImage;
+
     [SerializeField]
-    private Text ShotgunMod;
-    [SerializeField]
-    private Text RpgMod;
+    private Image GunElement;
 
     [SerializeField]
     private TextMeshProUGUI extraDamgage;
@@ -46,62 +28,57 @@ public class WeaponUI : MonoBehaviour
     [SerializeField]
     private WeaponSwitcher weaponSwitcher;
 
-    [SerializeField]
-    private ElementMain elementRifle;
-    [SerializeField]
-    private ElementMain elementShotgun;
-    [SerializeField]
-    private ElementMain elementRPG;
+    private bool HasRun;
+    //ElementMain element;
 
-    private bool allowShowRifle = false;
-    private bool allowShowShotgun = false;
-    private bool allowShowRpg = false;
+    ElementMain[] elements = new ElementMain[4];
 
+    [SerializeField]
+    GameObject guns;
+
+    private Vector3 RifleLocation;
+    private Vector3 ShotgunLocation;
+    private Vector3 RPGLocation;
+
+    private void Start()
+    {
+       // Weapon.onElementSwitch += CheckElement;
+        RifleLocation = new Vector3(rifleImage.transform.position.x, rifleImage.transform.position.y, 0);
+        ShotgunLocation = new Vector3(shotgunImage.transform.position.x, shotgunImage.transform.position.y, 0);
+        RPGLocation = new Vector3(rpgImage.transform.position.x, rpgImage.transform.position.y, 0);
+        elements = GetComponentsInChildren<ElementMain>();
+        Selector.color = Color.white;
+        //element = GetComponent<ElementMain>();
+    }
     // Update is called once per frame
     void Update()
     {
-        if (weaponSwitcher.curWeapon == Weapon.WeaponTypes.Rifle)
+        switch (weaponSwitcher.curWeapon)
         {
-            SelectorRifle.enabled = true;
-            if (allowShowRifle) { rifleEffective.enabled = true; rifleCurrent.enabled = true; RifleMod.enabled = true; }
-        }
-        else { SelectorRifle.enabled = false; rifleEffective.enabled = false; rifleCurrent.enabled = false; RifleMod.enabled = false; }
-
-        if (weaponSwitcher.curWeapon == Weapon.WeaponTypes.RPG)
-        {
-            SelectorRPG.enabled = true;
-            if (allowShowRpg) { rgpEffective.enabled = true; rpgCurrent.enabled = true; RpgMod.enabled = true; }
-        }
-        else { SelectorRPG.enabled = false; rgpEffective.enabled = false; rpgCurrent.enabled = false; RpgMod.enabled = false; }
-
-        if (weaponSwitcher.curWeapon == Weapon.WeaponTypes.Shotgun)
-        {
-            SelectorShotgun.enabled = true;
-            if (allowShowShotgun) { shotgunEffective.enabled = true; shotgunCurrent.enabled = true; ShotgunMod.enabled = true; }
-            }
-        else { SelectorShotgun.enabled = false; shotgunEffective.enabled = false; shotgunCurrent.enabled = false; ShotgunMod.enabled = false; }
-
-        //color changer for the Ui.
-        if (weaponSwitcher.curWeapon == Weapon.WeaponTypes.Rifle)
-        {
-            if (elementRifle.currentType == ElementMain.ElementType.Water) { SelectorRifle.color = ElementColors.WaterColor; rifleEffective.sprite = Fe; rifleCurrent.sprite = We; allowShowRifle = true; }
-            if (elementRifle.currentType == ElementMain.ElementType.Air) { SelectorRifle.color = ElementColors.AirColor; rifleEffective.sprite = We; rifleCurrent.sprite = Ae; allowShowRifle = true; }
-            if (elementRifle.currentType == ElementMain.ElementType.Fire) { SelectorRifle.color = ElementColors.FireColor; rifleEffective.sprite = Ae; rifleCurrent.sprite = Fe; allowShowRifle = true; }
-        }
-        if (weaponSwitcher.curWeapon == Weapon.WeaponTypes.Shotgun)
-        {
-            if (elementShotgun.currentType == ElementMain.ElementType.Water) { SelectorShotgun.color = ElementColors.WaterColor; shotgunEffective.sprite = Fe; shotgunCurrent.sprite = We; allowShowShotgun = true; }
-            if (elementShotgun.currentType == ElementMain.ElementType.Air){ SelectorShotgun.color = ElementColors.AirColor; shotgunEffective.sprite = We; shotgunCurrent.sprite = Ae; allowShowShotgun = true; }
-            if (elementShotgun.currentType == ElementMain.ElementType.Fire){ SelectorShotgun.color = ElementColors.FireColor; shotgunEffective.sprite = Ae; shotgunCurrent.sprite = Fe; allowShowShotgun = true; }
-        }
-        if (weaponSwitcher.curWeapon == Weapon.WeaponTypes.RPG)
-        {
-            if (elementRPG.currentType == ElementMain.ElementType.Water) { SelectorRPG.color = ElementColors.WaterColor; rgpEffective.sprite = Fe; rpgCurrent.sprite = We; allowShowRpg = true; }
-            if (elementRPG.currentType == ElementMain.ElementType.Air){ SelectorRPG.color = ElementColors.AirColor; rgpEffective.sprite = We; rpgCurrent.sprite = Ae; allowShowRpg = true; }
-            if (elementRPG.currentType == ElementMain.ElementType.Fire){ SelectorRPG.color = ElementColors.FireColor; rgpEffective.sprite = Ae; rpgCurrent.sprite = Fe; allowShowRpg = true; }
+            case Weapon.WeaponTypes.Rifle:
+                SwapWeapon(RifleLocation);
+                break;
+            case Weapon.WeaponTypes.Shotgun:
+                SwapWeapon(ShotgunLocation);
+                break;
+            case Weapon.WeaponTypes.RPG:
+                SwapWeapon(RPGLocation);
+                break;
+            default:
+                break;
         }
 
         extraDamgage.text = $"{(weaponSwitcher.ExtraDamage > 0 ? "+" : "")}{weaponSwitcher.ExtraDamage.ToString("0")}";
         extraSpeed.text = $"{(weaponSwitcher.ExtraSpeed > 0 ? "+" : "")}{(weaponSwitcher.ExtraSpeed.ToString())}";
+    }
+    void SwapWeapon(Vector3 gunPosition)
+    {
+        Selector.transform.position = gunPosition;
+    }
+    void CheckElement(ElementMain.ElementType elementType)
+    {
+        if (elementType == ElementMain.ElementType.Water) { Selector.color = ElementColors.WaterColor; GunElement.sprite = We; }
+        if (elementType == ElementMain.ElementType.Air) { Selector.color = ElementColors.AirColor; GunElement.sprite = Ae; }
+        if (elementType == ElementMain.ElementType.Fire) { Selector.color = ElementColors.FireColor; GunElement.sprite = Fe; }
     }
 }
