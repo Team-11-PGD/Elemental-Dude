32bd112142e1d939e050f387aac86ccf95433c27
RTosk <71409186+RTosk@users.noreply.github.com>
2022-02-08 23:07:52 +0100

    final push

--- Assets/Scripts/Enemies/EnemyColorController.cs
+++ Assets/Scripts/Enemies/EnemyColorController.cs
@@ -29,26 +29,26 @@ public class EnemyColorController : MonoBehaviour
         if (TypeRequirement(EnemyType.Spiker, ElementType.Water))
             meshFilter.mesh = ElementColors.instance.waterMesh;
 
-        if (TypeRequirement(EnemyType.Spiker, ElementType.Fire))
+        else if (TypeRequirement(EnemyType.Spiker, ElementType.Fire))
             meshFilter.mesh = ElementColors.instance.fireMesh;
 
-        if (TypeRequirement(EnemyType.Spiker, ElementType.Air))
+        else if (TypeRequirement(EnemyType.Spiker, ElementType.Air))
             meshFilter.mesh = ElementColors.instance.airMesh;
 
-        if (TypeRequirement(EnemyType.Slime, ElementType.Water))
+        else if (TypeRequirement(EnemyType.Slime, ElementType.Water))
             slimeRender.material = ElementColors.instance.WaterSlime;
 
-        if (TypeRequirement(EnemyType.Slime, ElementType.Fire))
+        else if (TypeRequirement(EnemyType.Slime, ElementType.Fire))
             slimeRender.material = ElementColors.instance.FireSlime;
 
-        if (TypeRequirement(EnemyType.Slime, ElementType.Air))
+        else if (TypeRequirement(EnemyType.Slime, ElementType.Air))
             slimeRender.material = ElementColors.instance.AirSlime;
     }
     public void SlimeRandom()
     {
         slimeRender = GetComponent<Renderer>();
         if (randomValue == 0) elementMain.currentType = ElementType.Air;
-        if (randomValue == 1) elementMain.currentType = ElementType.Water;
-        if (randomValue == 2) elementMain.currentType = ElementType.Fire;
+        else if (randomValue == 1) elementMain.currentType = ElementType.Water;
+        else if (randomValue == 2) elementMain.currentType = ElementType.Fire;
     }
 }
--- Assets/Scripts/Enemies/SlimeStates/SlimeMove.cs
+++ Assets/Scripts/Enemies/SlimeStates/SlimeMove.cs
@@ -7,7 +7,19 @@ public class SlimeMove : SlimeState
     NavMeshAgent agent;
     [SerializeField]
     float stopRange = 2f;
+    [SerializeField]
+    Rigidbody rigidbody;
+    private bool jumping, executed;
+    private float jumpTimer = 1f, deltaTimer;
+    private float jumpCooldown, deltaCooldown;
+    private float randomValue;
+    public int jumpForce = 2000;
 
+    private void Start()
+    {
+        randomValue = Random.Range(0.5f, 1.1f);
+        jumpCooldown = randomValue;
+    }
     public override void Enter(int previousStateId)
     {
         base.Enter(previousStateId);
@@ -17,11 +29,39 @@ public class SlimeMove : SlimeState
 
     void Update()
     {
-        agent.SetDestination(slimeAI.playerModel.position);
-        if (Vector3.Distance(slimeAI.playerModel.position, transform.position) <= stopRange)
+        deltaTimer += Time.deltaTime;
+        TimedJumping();// calls for the jump
+
+        if (!jumping)//if slime not jumping it will move to the player.
         {
-            agent.SetDestination(transform.position);
-            context.TransitionTo(EnemyAI.StateOptions.Attacking);
+            agent.SetDestination(slimeAI.playerModel.position);
+            if (Vector3.Distance(slimeAI.playerModel.position, transform.position) <= stopRange)
+            {
+                agent.SetDestination(transform.position);
+                context.TransitionTo(EnemyAI.StateOptions.Attacking);
+            }
+        }
+    }
+    private void TimedJumping() //timed jumps, will jump in set interfalls. it will disable the agent for a short while.
+    {
+        if (deltaTimer >= jumpTimer)
+        {
+            deltaCooldown += Time.deltaTime;
+            jumping = true;
+            agent.enabled = false;
+            if (!executed)
+            {
+                rigidbody.AddForce(0, jumpForce, 0);
+                executed = true;
+            }
+            if (deltaCooldown >= jumpCooldown) deltaTimer = 0;
+        }
+        else
+        {
+            jumping = false;
+            executed = false;
+            agent.enabled = true;
+            deltaCooldown = 0;
         }
     }
 }
